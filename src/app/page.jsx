36e45226c6c51e85d5539b32a77d419f81d7376c
ehasun15019 <ehasunulislam0202@gmail.com>
2026-02-17 00:53:47 +0600

    complete Trusted section in Hero folder

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,10 +1,12 @@
 import HomeHero from "@/Components/Hero-Section/Home-Hero/HomeHero";
+import Trusted from "@/Components/Hero-Section/Trusted/Trusted";
 import Image from "next/image";
 
 export default function Home() {
   return (
    <div>
     <HomeHero />
+    <Trusted />
    </div>
   );
 }

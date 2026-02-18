75336cd7d553bea90ad5b7bff76cb2c6baa1be73
Sadia1609 <sadia.rahman160920@gmail.com>
2026-02-18 16:37:53 +0600

    added daisyui configuration in tailwind.config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
@@ -19,4 +20,29 @@ module.exports = {
     },
   },
   plugins: [require("daisyui")],
+
+  //daisyUI configuration
+  daisyui: {
+    themes: [
+      {
+        secureexam: {
+          primary: "#0D7C66",
+          "primary-content": "#ffffff",
+          secondary: "#41B3A2",
+          accent: "#D7C3F1",
+          neutral: "#f1f5f9",
+          "base-100": "#ffffff",
+          "base-200": "#f8fafc",
+          "base-300": "#e2e8f0",
+          info: "#3b82f6",
+          success: "#10b981",
+          warning: "#f59e0b",
+          error: "#ef4444",
+        },
+      },
+    ],
+    base: true,
+    styled: true,
+    utils: true,
+  },
 };

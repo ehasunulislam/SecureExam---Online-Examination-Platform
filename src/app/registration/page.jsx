648df8878e95e75160d84e035a49dd81d8fb9793
Sadia1609 <sadia.rahman160920@gmail.com>
2026-02-18 19:17:09 +0600

    added decorative  background icons on register page

--- src/app/registration/page.jsx
+++ src/app/registration/page.jsx
@@ -1,21 +1,18 @@
+import { GraduationCap, Shield } from "lucide-react";
 import React from "react";
 
-const Registration = () => {
+const registration = () => {
   return (
-    <div>
-      <fieldset className="fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4">
-        <legend className="fieldset-legend">Login</legend>
-
-        <label className="label">Email</label>
-        <input type="email" className="input" placeholder="Email" />
-
-        <label className="label">Password</label>
-        <input type="password" className="input" placeholder="Password" />
-
-        <button className="btn btn-neutral mt-4">Login</button>
-      </fieldset>
+    <div className="min-h-screen flex items-center justify-center p-4 relative overflow-hidden bg-linear-to-br from-slate-50 to-slate-100">
+      {/* Decorative Background Icons */}
+      <div className="fixed top-0 right-0 w-96 h-96 opacity-5 pointer-events-none">
+        <Shield className="w-70 h-70 text-[#0D7C66] absolute top-10 right-10 rotate-12" />
+      </div>
+      <div className="fixed bottom-0 left-0 w-96 h-96 opacity-5 pointer-events-none">
+        <GraduationCap className="w-70 h-70 text-[#41B3A2] absolute bottom-10 left-10 -rotate-12" />
+      </div>
     </div>
   );
 };
 
-export default Registration;
+export default registration;

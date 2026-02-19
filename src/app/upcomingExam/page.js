ca2fae69adb20d63129380b567c07223fad982ca
bakia <bakia.nimmy@gmail.com>
2026-02-19 11:05:18 +0600

    Created a upcoming exam page

---
+++ src/app/upcomingExam/page.js
@@ -0,0 +1,9 @@
+export default function UpcomingExam() {
+  return (
+    <main className="min-h-screen flex items-center justify-center bg-gray-50">
+      <p className="text-xl font-semibold text-gray-700">
+        Upcoming Exam
+      </p>
+    </main>
+  );
+}

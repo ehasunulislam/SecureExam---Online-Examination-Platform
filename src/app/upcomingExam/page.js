2a28c4f520652d6dddfb2d0fc5c00f19751a6c26
bakia <bakia.nimmy@gmail.com>
2026-02-19 11:10:05 +0600

    created a basic feature and fake json for that

--- src/app/upcomingExam/page.js
+++ src/app/upcomingExam/page.js
@@ -3,6 +3,7 @@ export default function UpcomingExam() {
     <main className="min-h-screen flex items-center justify-center bg-gray-50">
       <p className="text-xl font-semibold text-gray-700">
         Upcoming Exam
+        
       </p>
     </main>
   );
